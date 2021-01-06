b1d4ee8973c7b61dc2509182265539d08ce1c793
t03jam8 <grovejam@gmail.com>
2021-01-06 21:29:29 +0100

    New tests for python

---
+++ tests/test_add.py
@@ -0,0 +1,12 @@
+import unittest
+from utils.add import add
+
+
+class TestAdd(unittest.TestCase):
+    def test_add_int(self):
+        result = add(5, 5)
+        self.assertEqual(result, 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
