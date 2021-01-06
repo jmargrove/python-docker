2aaf2290a7d654554330056b3ac2d7033beccb73
t03jam8 <grovejam@gmail.com>
2021-01-06 21:58:20 +0100

    tests working localy

--- tests/test_add.py
+++ tests/test_add.py
@@ -6,3 +6,7 @@ class TestAdd(unittest.TestCase):
     def test_add_int(self):
         result = add(5, 5)
         self.assertEqual(result, 10)
+
+    # def test_to_fail(self):
+    #     result = add(5, 5)
+    #     self.assertEqual(result, 1)
